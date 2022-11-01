464763ad699678a7386f49672254ebced6848a85
amo-tech <kaomi.x1003@gmail.com>
2022-11-01 09:25:55 +0900

    JSX

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,48 @@ import { NodeBuilderFlags } from "typescript";
 import "./styles.css";
 
 export const App = () => {
-  return null;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>
+            <div>
+            <span>123</span>
+            <button>完了</button>
+            <button>削除</button>
+            </div>
+          </li>
+          <li>
+            <div>
+            <span>456</span>
+            <button>完了</button>
+            <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div>
+      <p>完了のTODO</p>
+        <ul>
+          <li>
+            <div>
+            <span>aaa</span>
+            <button>戻す</button>
+            </div>
+          </li>
+          <li>
+            <div>
+            <span>bbb</span>
+            <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
